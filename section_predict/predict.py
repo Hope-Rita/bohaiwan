1ad49809ac9d10fc1aba690b41619cecd51818ba
Wentao Li <lwt1406@buaa.edu.cn>
2020-11-10 13:44:36 +0800

    section predict adjust parameters

--- section_predict/predict.py
+++ section_predict/predict.py
@@ -1,7 +1,3 @@
-import numpy as np
-import platform
-
-
 from utils.config import Config
 config_path = '../section_predict/config.json'
 conf = Config(config_path)
@@ -12,27 +8,49 @@ else:
 pred_section = conf.get_config('predict-section')
 
 
-import utils.pred_utils as pu
-from baseline import lr
-from baseline import mlp
+import platform
+import os
+import pandas as pd
+from importlib import reload
 from baseline import recurrent
-from baseline import svr
-from baseline import xgb
-from baseline import rf
-from baseline import knn
 from section_predict import gen_dataset
 from utils import normalization
 from utils import data_process
-from utils import draw_pic
 from utils import metric
 
 
-def predict_one_section(filename, data, func):
+def adjust_para(filename, data, func):
+    hidden_size_range = range(5, 85, 5)
+    lr_range = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3]
+
+    res = []
+    for hidden_size in hidden_size_range:
+        recurrent_model = func.__name__.split('_')[0]
+        conf.modify_config('model-parameters', 'recurrent', f'{recurrent_model}-hidden-size', new_val=hidden_size)
+        metrics = {'hidden_size': hidden_size}
+        for lr in lr_range:
+            print(f'hidden_size: {hidden_size}, lr: {lr}')
+            conf.modify_config('model-parameters', 'recurrent', 'learning-rate', new_val=lr)
+            reload(recurrent)
+            pred_res = predict_one_section(filename, data, func, dump_csv=False)
+            df = pd.DataFrame(pred_res)
+            metrics[lr] = data_process.avg(df.loc[:, 'RMSE'])
+        res.append(metrics)
+
+    csv_name = func.__name__.split('_')[0] + f'_{gen_dataset.future_days}day' + '_' + pred_section + '_'
+    csv_name += filename.split('\\')[-1] if platform.system() is 'Windows' else filename.split('/')[-1]
+    res_frame = pd.DataFrame(res)
+    res_frame.to_csv(path_or_buf=os.path.join('adjust_para', csv_name), index=False)
+
+
+def predict_one_section(filename, data, func, dump_csv=True):
     """
     预测一个 section 的结果
     @param filename: 存放数据的文件
     @param data: 预测用的数据
     @param func: 预测用的方法
+    @param dump_csv: 是否将预测结果的指标写入到 CSV 中
+    @return 该 section 内每个传感器的预测指标，格式为 List(dict)
     """
     x_train, y_train, x_test, y_test, sensors = data
 
@@ -52,15 +70,17 @@ def predict_one_section(filename, data, func):
         # print(sensors[i], sensor_metric)
         section_metrics.append(sensor_metric)
 
-    # 写入 CSV 文件。系统不同，处理方式不一样
-    csv_name = func.__name__.split('_')[0] + f'_{gen_dataset.future_days}day' + '_' + pred_section + '_'
-    csv_name += filename.split('\\')[-1] if platform.system() is 'Windows' else filename.split('/')[-1]
+    if dump_csv:  # 写入 CSV 文件。系统不同，处理方式不一样
+        csv_name = func.__name__.split('_')[0] + f'_{gen_dataset.future_days}day' + '_' + pred_section + '_'
+        csv_name += filename.split('\\')[-1] if platform.system() is 'Windows' else filename.split('/')[-1]
+        data_process.dump_csv(pred_res_dir, csv_name, section_metrics, average_func=data_process.avg)
 
-    data_process.dump_csv(pred_res_dir, csv_name, section_metrics, average_func=data_process.avg)
+    return section_metrics
 
 
 if __name__ == '__main__':
     pred_target = conf.get_config('predict-target')
     pred_target_filename = conf.get_data_loc(pred_target)
     pred_data = gen_dataset.load_one_section(filename=pred_target_filename, section_name=pred_section)
-    predict_one_section(pred_target_filename, pred_data, recurrent.rnn_section_predict)
+    # predict_one_section(pred_target_filename, pred_data, recurrent.rnn_section_predict)
+    adjust_para(pred_target_filename, pred_data, recurrent.rnn_section_predict)
